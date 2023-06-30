44f8df6010439d574b15e5ed76dca768bd53a6a2
VijittaAlexander <vijitta.alexander@kanini.com>
2023-06-30 15:31:22 +0530

    mobx-update

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,106 +1,104 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Container} from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
-  const[activities, setActivities] = useState<Activity[]>([]);
-  const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editmode, setEditMode] = useState(false);
-  const[loading, setLoading] = useState(true);
-  const[submitting, setSubmitting] = useState(false);
+  const {activityStore} = useStore();
+
+  //const[activities, setActivities] = useState<Activity[]>([]);
+  // const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  // const [editmode, setEditMode] = useState(false);
+ // const[submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
   //  axios.get<Activity[]>('http://localhost:5000/api/Activities')
-  agent.Activities.list()
-    .then(response => {
-      let activities : Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-      console.log(response);
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+  // agent.Activities.list()
+  //   .then(response => {
+  //     let activities : Activity[] = [];
+  //     response.forEach(activity => {
+  //       activity.date = activity.date.split('T')[0];
+  //       activities.push(activity);
+  //     })
+  //     console.log(response);
+  //     setActivities(activities);
+  //     setLoading(false);
+  //   })
+  activityStore.loadActivities();
+  }, [activityStore])
 
-  function handleSelectedActivity(id: string){
-     setSelectedActivity(activities.find(x => x.id === id))
-  }
+  // function handleSelectedActivity(id: string){
+  //    setSelectedActivity(activities.find(x => x.id === id))
+  // }
 
-  function handleCancelActivity(){
-    setSelectedActivity(undefined);
-  }
+  // function handleCancelActivity(){
+  //   setSelectedActivity(undefined);
+  // }
 
-  function handleFormOpen(id?: string){
-    id ? handleSelectedActivity(id) : handleCancelActivity();
-    setEditMode(true);
-  }
+  // function handleFormOpen(id?: string){
+  //   id ? handleSelectedActivity(id) : handleCancelActivity();
+  //   setEditMode(true);
+  // }
 
-  function handleFormClose() {
-    setEditMode(false);
-  }
+  // function handleFormClose() {
+  //   setEditMode(false);
+  // }
 
-  function handleCreateOrEditActivity(activity: Activity){
-    setSubmitting(true);
-    if(activity.id){
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity)
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]);
-        setSelectedActivity(activity)
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
+  // function handleCreateOrEditActivity(activity: Activity){
+  //   setSubmitting(true);
+  //   if(activity.id){
+  //     agent.Activities.update(activity).then(() => {
+  //       setActivities([...activities.filter(x => x.id !== activity.id), activity])
+  //       setSelectedActivity(activity)
+  //       setEditMode(false);
+  //       setSubmitting(false);
+  //     })
+  //   } else {
+  //     activity.id = uuid();
+  //     agent.Activities.create(activity).then(() => {
+  //       setActivities([...activities, activity]);
+  //       setSelectedActivity(activity)
+  //       setEditMode(false);
+  //       setSubmitting(false);
+  //     })
+  //   }
+  //}
     // activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
     // : setActivities([...activities, {...activity, id: uuid()}]);
     // setEditMode(false);
     // setSelectedActivity(activity);
-  }
+ 
 
-  function handleDeleteActivity(id: string){
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })
+  // function handleDeleteActivity(id: string){
+  //   setSubmitting(true);
+  //   agent.Activities.delete(id).then(() => {
+  //     setActivities([...activities.filter(x => x.id !== id)]);
+  //     setSubmitting(false);
+  //   })
     
-  }
+  // }
 
-  if (loading) return <LoadingComponent content='Loading app'/>
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
-      <NavBar openForm={handleFormOpen} />
+      <NavBar />
       <Container style={{marginTop: '7em'}}>
        <ActivityDashboard 
-        activities={activities} 
-        selectedActivity = {selectedActivity}
-        selectActivity = {handleSelectedActivity}
-        cancelSelectActivity = {handleCancelActivity}
-        editMode ={editmode}
-        openForm = {handleFormOpen}
-        closeForm = {handleFormClose}
-        createOrEdit={handleCreateOrEditActivity}
-        deleteActivity ={handleDeleteActivity}
-        submitting = {submitting}
+        //activities={activityStore.activities} 
+        // selectedActivity = {selectedActivity}
+        // editMode ={editmode}
+        //createOrEdit={handleCreateOrEditActivity}
+        // deleteActivity ={handleDeleteActivity}
+        //submitting = {submitting}
         />
       </Container>
      </>
   );
 }
 
-export default App;
+export default observer(App);
