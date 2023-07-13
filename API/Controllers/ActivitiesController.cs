04ce7e934789ff88166a054e60165bcc5eb02fab
VijittaAlexander <vijitta.alexander@kanini.com>
2023-07-13 15:10:50 +0530

    Entity relationship changes

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace API.Controllers
 {
@@ -25,6 +26,7 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
+        [Authorize(Policy ="IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateActivity(Guid id, Activity activity)
         {
@@ -32,10 +34,17 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Update.Command{Activity = activity}));
         }
 
+        [Authorize(Policy ="IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult>DeleteActivity(Guid id)
         {
            return HandleResult(await Mediator.Send(new Delete.Command{Id = id})); 
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command{Id = id}));
         }
     }
 }
