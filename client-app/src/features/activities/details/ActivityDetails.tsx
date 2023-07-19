eca970773b0a6a4b78ea7c32e0196026dc23b65f
VijittaAlexander <vijitta.alexander@kanini.com>
2023-07-19 18:24:28 +0530

    implementing SignalR

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { observer } from 'mobx-react-lite';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import ActivityDetailedHeader from './ActivityDetailedHeader';
 import ActivityDetailedInfo from './ActivityDetailedInfo';
@@ -18,12 +18,13 @@ import ActivityDetailedSidebar from './ActivityDetailedSidebar';
 export default observer(function ActivityDetails() {
  
     const {activityStore} = useStore();
-    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial, clearSelectedActivity} = activityStore;
     const {id} = useParams();
 
     useEffect(() => {
       if(id) loadActivity(id);
-    }, [id,loadActivity])
+      return () => clearSelectedActivity();
+    }, [id,loadActivity,clearSelectedActivity])
 
     if(loadingInitial || !activity) return <LoadingComponent/>;
 
@@ -32,7 +33,7 @@ export default observer(function ActivityDetails() {
         <Grid.Column width={10}>
           <ActivityDetailedHeader activity={activity} />
           <ActivityDetailedInfo activity={activity} />
-          <ActivityDetailedChat />
+          <ActivityDetailedChat activityId={activity.id} />
         </Grid.Column>
         <Grid.Column width={6}>
         <ActivityDetailedSidebar activity={activity} />
